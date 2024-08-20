fa794e6f75e5030eb545db21aa3ffea3d7f0efed
ajitj <ajitk123@gmail.com>
2024-08-20 18:52:47 +0530

    Nootbook sample

--- src/main/java/com/space/ajit/universe/model/universe/World.java
+++
@@ -1,9 +0,0 @@
-package com.space.ajit.universe.model.universe;
-
-import lombok.Data;
-import org.springframework.data.mongodb.core.mapping.Document;
-
-@Data
-@Document
-public class World {
-}
--- src/main/java/com/space/ajit/universe/repository/UniverseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.space.ajit.universe.repository;
-
-import com.space.ajit.universe.model.universe.Universe;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UniverseRepository extends MongoRepository<Universe,String> {
-}
